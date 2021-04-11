1b905ae8c7b8a82473632d0d43b81fd85931a736
Shawn Gonsalves <55061053+shawngonsalves@users.noreply.github.com>
2021-04-11 16:07:51 -0700

    No. 1189

---
+++ Maximum Number of Balloons.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        d = {'b':0,'a':0,'l':0,'o':0,'n':0}
+        for item in text:
+            if item in ('b','a','l','l','o','o','n'):
+                d[item] += 1
+        if len(d) < 5 or d['l'] == 1 or d['o'] == 1:
+            return 0
+        
+        print(d)
+        d['l'] = d['l'] // 2
+        d['o'] = d['o'] // 2
+        return min(d.values())
+        print(d)
