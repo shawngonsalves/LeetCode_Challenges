d99591f29199a66ac1c5d5f45e875a73a537fd27
Shawn Gonsalves <55061053+shawngonsalves@users.noreply.github.com>
2021-05-09 14:04:02 -0700

    Easy_ShuffleArray
    Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
    
    Return the array in the form [x1,y1,x2,y2,...,xn,yn].
    
     
    
    Example 1:
    
    Input: nums = [2,5,1,3,4,7], n = 3
    Output: [2,3,5,4,1,7] 
    Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].

---
+++ ShuffleArray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+
+        
+        result = []
+        for i in range(n):
+            result.append(nums[i])
+            result.append(nums[n+i])
+
+        return result
