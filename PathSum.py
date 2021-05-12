1288523804f059458034a45e5f0888ed747293e1
Shawn Gonsalves <55061053+shawngonsalves@users.noreply.github.com>
2021-05-11 21:10:43 -0700

    Easy_PathCrossing
    Given a string path, where path[i] = 'N', 'S', 'E' or 'W', each representing moving one unit north, south, east, or west, respectively. You start at the origin (0, 0) on a 2D plane and walk on the path specified by path.
    
    Return true if the path crosses itself at any point, that is, if at any time you are on a location you have previously visited. Return false otherwise.
    
     
    
    Example 1:
    
    
    Input: path = "NES"
    Output: false 
    Explanation: Notice that the path doesn't cross any point more than once.

---
+++ PathSum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        x, y = 0, 0
+        visited = {(0, 0)}
+        
+        for word in path:
+            if word == 'N':
+                y += 1
+            elif word == 'S':                
+                y -= 1
+            elif word == 'E':
+                x += 1
+            else:
+                x -= 1
+            
+            if (x,y) in visited:
+                return True            
+            visited.add((x, y))
+            
+        return False
+        
