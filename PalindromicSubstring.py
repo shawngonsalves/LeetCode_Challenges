8758d513d7d272dd9b8592d4ff6dacebc1dd1146
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-01 22:49:08 -0700

    .

--- PalindromicSubstring.py
+++ PalindromicSubstring.py
@@ -1,3 +1,19 @@
+'''
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+'''
+
 
 
 
