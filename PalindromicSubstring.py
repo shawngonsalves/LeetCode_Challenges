57c0ca3f2d7df2cd53ef546b821084d2fdac1cd1
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-01 22:49:49 -0700

    Medium_PalindromicSubstring

--- PalindromicSubstring.py
+++ PalindromicSubstring.py
@@ -1,8 +1,6 @@
 '''
 Given a string s, return the number of palindromic substrings in it.
-
 A string is a palindrome when it reads the same backward as forward.
-
 A substring is a contiguous sequence of characters within the string.
 
  
@@ -14,16 +12,28 @@ Output: 3
 Explanation: Three palindromic strings: "a", "b", "c".
 '''
 
-
-
-
-
-
-
-
-
-
-
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        
+        def countPalindrome(l,r,s):
+            res = 0
+            while l >= 0 and r < len(s) and s[l]==s[r]:
+                res+=1
+                l-=1
+                r+=1
+            return res
+            
+        
+        for i in range(len(s)):
+            l,r = i, i
+            res+= countPalindrome(l,r,s)
+            res+=countPalindrome(i,i+1,s)
+            
+        return res
+    
+        
+                
 
 '''
 Success
