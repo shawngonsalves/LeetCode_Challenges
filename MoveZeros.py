b3e18bb79dbf917b80984182e9772076b28c0675
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-01 23:54:32 -0700

    Easy_MoveZeros_Solution

--- MoveZeros.py
+++ MoveZeros.py
@@ -8,3 +8,19 @@ Example 1:
 Input: nums = [0,1,0,3,12]
 Output: [1,3,12,0,0]
 '''
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count = nums.count(0)
+        print(count)
+        for i in range(count):
+            if 0 in nums:
+                nums.remove(0)
+       
+        for i in range(count):
+            nums.append(0)
+
+        return nums
