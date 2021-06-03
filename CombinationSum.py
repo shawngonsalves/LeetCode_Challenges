091744fbba752b002a2b26ab1ad5f675adf53b09
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-02 20:48:33 -0700

    .

---
+++ CombinationSum.py
@@ -0,0 +1,25 @@
+'''
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+'''
+
+
+
+'''
+Success
+Details 
+Runtime: 136 ms, faster than 16.02% of Python3 online submissions for Combination Sum.
+Memory Usage: 14.5 MB, less than 23.68% of Python3 online submissions for Combination Sum.
+'''
