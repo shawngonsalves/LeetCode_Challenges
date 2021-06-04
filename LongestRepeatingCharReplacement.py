bd25a1e64f9c760b4bea7e759f0b58d666061a7b
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-04 16:27:06 -0700

    Medium_LongestRepeatingCharacterReplacement

--- LongestRepeatingCharReplacement.py
+++ LongestRepeatingCharReplacement.py
@@ -25,7 +25,7 @@ class Solution:
         l = 0
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
-            
+            maxF = max(maxF, count[s[r]])
             while (r-l +1) - maxF > k:
                 count[s[l]]-=1
                 l+=1
