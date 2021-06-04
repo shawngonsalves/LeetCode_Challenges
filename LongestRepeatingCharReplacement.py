cc72063a5b2b2dc06ed6f3e280a09c8a2af9cda8
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-04 16:25:16 -0700

    Medium_LongestRepeatingCharReplacement

--- LongestRepeatingCharReplacement.py
+++ LongestRepeatingCharReplacement.py
@@ -17,6 +17,26 @@ The substring "BBBB" has the longest repeating letters, which is 4.
 '''
 
 
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        count = {}
+        
+        l = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            
+            while (r-l +1) - max(count.values()) > k:
+                count[s[l]]-=1
+                l+=1
+                
+            res = max(res, r-l+1)
+        return res
+
+
+
+
+
 '''
 Success
 Details 
