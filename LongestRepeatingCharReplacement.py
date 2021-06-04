3f560aa589b6798f59a80be95b4b634241728e13
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-04 16:26:06 -0700

    .

--- LongestRepeatingCharReplacement.py
+++ LongestRepeatingCharReplacement.py
@@ -21,12 +21,12 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         res = 0
         count = {}
-        
+        maxF = 0
         l = 0
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
             
-            while (r-l +1) - max(count.values()) > k:
+            while (r-l +1) - maxF > k:
                 count[s[l]]-=1
                 l+=1
                 
