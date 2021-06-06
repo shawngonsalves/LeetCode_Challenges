f862c6841ff3a50ec3b8b4b043b077b9a97f3ee4
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-05 19:20:04 -0700

    Easy_ClimbingStairs

--- ClimbingStairs.py
+++ ClimbingStairs.py
@@ -10,3 +10,29 @@ Explanation: There are two ways to climb to the top.
 1. 1 step + 1 step
 2. 2 steps
 '''
+class Solution:
+    def climbStairs(self, n: int) -> int:  
+        if n < 2:
+            return 1
+        '''step = [0 for _ in range (n)]
+        step[0] = 1
+        step[1] = 2
+        for i in range(2, n):
+            step[i] = step[i-1]+step[i-2]
+        return step[-1]'''
+        
+        one = 1
+        two = 1
+        
+        for i in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
+            
+'''
+Success
+Details 
+Runtime: 24 ms, faster than 93.41% of Python3 online submissions for Climbing Stairs.
+Memory Usage: 14.1 MB, less than 89.70% of Python3 online submissions for Climbing Stairs.
+'''
