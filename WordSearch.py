96e95c33ee39ec461d11937cb257aa719acc451f
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-09 21:26:17 -0700

    Medium_WordSearch

---
+++ WordSearch.py
@@ -0,0 +1,13 @@
+'''
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+ 
+
+Example 1:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+'''
+
