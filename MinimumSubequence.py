7a02db7285e0369210568dd2a7def9df3e17031e
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-13 16:02:20 -0700

    Easy_MinSubsequence

--- MinimumSubequence.py
+++ MinimumSubequence.py
@@ -23,3 +23,28 @@ Output: [7,7,6]
 Explanation: The subsequence [7,7] has the sum of its elements equal to 14 which is not strictly greater than the sum of elements not included (14 = 4 + 4 + 6). 
 Therefore, the subsequence [7,6,7] is the minimal satisfying the conditions. Note the subsequence has to returned in non-decreasing order.  
 '''
+
+class Solution:
+    def minSubsequence(self, nums: List[int]) -> List[int]:
+        res = []
+        if len(nums)==1:
+            return nums
+        nums = sorted(nums, reverse =True)
+        print(nums)
+        
+        res.append(nums[0])
+        print(res)
+        i = 1
+        while len(res) <= len(nums):
+            if sum(res) > sum(nums)- sum(res):
+                print(res)
+                return res
+            else:
+                res.append(nums[i])
+                print(res)
+                i+=1
+        
+'''
+Runtime: 76 ms, faster than 37.56% of Python3 online submissions for Minimum Subsequence in Non-Increasing Order.
+Memory Usage: 14.4 MB, less than 35.12% of Python3 online submissions for Minimum Subsequence in Non-Increasing Order.
+'''
