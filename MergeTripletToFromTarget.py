6755710de8188c29ae2186ddcdae4e7b92730f40
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-14 13:51:40 -0700

    .

--- MergeTripletToFromTarget.py
+++ MergeTripletToFromTarget.py
@@ -29,3 +29,16 @@ Input: triplets = [[3,4,5],[4,5,6]], target = [3,2,5]
 Output: false
 Explanation: It is impossible to have [3,2,5] as an element because there is no 2 in any of the triplets.
 '''
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        good = set()
+        
+        for t in triplets:
+            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
+                continue
+                
+            for i, v in enumerate(t):
+                if v ==target[i]:
+                    good.add(i)
+                    
+        return len(good) ==3
