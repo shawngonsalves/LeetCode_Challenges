759f3f9d4cef2e34b10724202452c40a343aaec0
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-14 13:32:52 -0700

    Medium_MaxRemovableChars_Question

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -15,3 +15,26 @@ Example 2:
 Input: x = -123
 Output: -321
 '''
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x == 0:
+	        return 0
+        if x > 0:
+	        rev = str(x)
+	        rev = rev[::-1]
+	        if int(rev) > 2 ** 31:
+		        return 0
+	        return (int(rev))
+        if x < 0:
+	        x = x * -1
+	        rev = str(x)
+	        rev = rev[::-1]
+	        if int(rev) > 2 ** 31:
+		        return 0
+	        return (int(rev) * -1)
+
+'''
+Runtime: 16 ms, faster than 99.97% of Python3 online submissions for Reverse Integer.
+Memory Usage: 14.2 MB, less than 43.19% of Python3 online submissions for Reverse Integer.
+'''
