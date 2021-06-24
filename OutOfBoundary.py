d937e043c847bf96e751913d0409e678bccf3904
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-24 13:13:38 -0700

    Medium_OutOfBoundary

--- OutOfBoundary.py
+++ OutOfBoundary.py
@@ -16,3 +16,23 @@ Example 2:
 Input: m = 1, n = 3, maxMove = 3, startRow = 0, startColumn = 1
 Output: 12
 '''
+
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        memo = {}
+        
+        def rec(r,c, M):
+            if r < 0 or c < 0 or r>= m or c>=n:
+                return 1
+            if M == 0:
+                return 0
+            if (r,c,M) in memo: return memo[(r,c,M)]
+            ans = 0
+            for x,y in [(1,0),(-1,0),(0,1),(0,-1)]:
+                ans += rec(r+x, c+y, M-1)
+                
+            memo[(r,c,M)] = ans
+            return ans
+        
+        return rec(startRow,startColumn, maxMove)%(10**9+7)
+            
