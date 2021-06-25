63c348ce24fc321ad7f8f5295e6595d4317e57f2
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-25 11:07:47 -0700

    Medium_SortColors

--- SortColors.py
+++ SortColors.py
@@ -21,3 +21,32 @@ Input: nums = [0]
 Output: [0]
 '''
 
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0 
+        r = len(nums)-1
+        
+        i = 0
+        
+        def swap(i,j):
+            temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+        
+        while i <=r:
+            if nums[i] ==0:
+                swap(i,l)
+                l+=1
+            if nums[i] ==2:
+                swap(i,r)
+                r-=1
+                i-=1
+            i+=1
+
+'''
+Runtime: 32 ms, faster than 76.07% of Python3 online submissions for Sort Colors.
+Memory Usage: 14.1 MB, less than 73.01% of Python3 online submissions for Sort Colors.
+'''
