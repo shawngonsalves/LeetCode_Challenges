ab94633961f936ec479ca0a9bc3848bf5f32b25b
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-26 17:07:12 -0700

    Medium_LongestIncreasingSubsequence

--- LongestIncreasingSubsequence.py
+++ LongestIncreasingSubsequence.py
@@ -16,3 +16,16 @@ Input: nums = [0,1,0,3,2,3]
 Output: 4
 '''
 
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        lis = [1] *len(nums)
+        
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    lis[i] = max(lis[i], 1+lis[j])
+        return max(lis)
+
+'''
+
+'''
