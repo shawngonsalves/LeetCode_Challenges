f95623c888ab518a994bbffd7fc5d17405725dbd
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-27 22:12:11 -0700

    Hard_WordLadder

--- WordLadder.py
+++ WordLadder.py
@@ -23,3 +23,35 @@ Output: 0
 Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
 '''
 
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        nei = collections.defaultdict(list)
+        print(nei)
+        
+        wordList.append(beginWord)
+        
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j]+ "*"+ word[j+1:]
+                nei[pattern].append(word)
+                
+        print(nei)
+        
+        visit = set(beginWord)
+        q = deque([beginWord])
+        res = 1
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word== endWord:
+                    return res
+                for j in range(len(word)):
+                    pattern = word[:j]+ "*"+ word[j+1:]
+                    for neiWord in nei[pattern]:
+                        if neiWord not in visit:
+                            visit.add(neiWord)
+                            q.append(neiWord)
+            res+=1
+        return 0
