0183f6053f6eca53687dc7289fbf7ec2cff49831
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-29 12:40:37 -0700

    Medium_MaxPointsFromCards

--- MaxPointsFromCards.py
+++ MaxPointsFromCards.py
@@ -20,3 +20,19 @@ Input: cardPoints = [2,2,2], k = 2
 Output: 4
 Explanation: Regardless of which two cards you take, your score will always be 4.
 '''
+
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        l = 0
+        r = len(cardPoints) - k
+        total = sum(cardPoints[r:])
+        res = total
+        
+        while(r < len(cardPoints)):
+            total += (cardPoints[l] - cardPoints[r])
+            res = max(res, total)
+            l+=1
+            r+=1
+            
+        return res
+            
