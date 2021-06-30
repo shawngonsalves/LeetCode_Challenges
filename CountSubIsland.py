92104fa0c4c2e027a299df059cc0cac108af84fe
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-30 11:30:14 -0700

    Medium_CountSubIsland

--- CountSubIsland.py
+++ CountSubIsland.py
@@ -23,3 +23,28 @@ Output: 2
 Explanation: In the picture above, the grid on the left is grid1 and the grid on the right is grid2.
 The 1s colored red in grid2 are those considered to be part of a sub-island. There are two sub-islands.
 '''
+
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        ROWS = len(grid1)
+        COLS = len(grid1[0])
+        visit = set()
+        def dfs(r,c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r,c) in visit or grid2[r][c] ==0:
+                return True
+            visit.add((r,c))
+            res = True
+            if grid1[r][c] == 0:
+                res = False
+            res = dfs(r+1,c) and res
+            res = dfs(r-1,c) and res
+            res = dfs(r,c+1) and res
+            res = dfs(r,c-1) and res
+            return res
+        count = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid2[r][c] == 1 and (r,c) not in visit and dfs(r,c):
+                    count += 1
+                    
+        return count
