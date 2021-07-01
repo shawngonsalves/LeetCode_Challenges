d075da400effe610d2ef3ef07fbb8fd0af644f5e
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-30 19:24:59 -0700

    Medium_Subsets

--- Subsets.py
+++ Subsets.py
@@ -14,3 +14,22 @@ Example 2:
 Input: nums = [0]
 Output: [[],[0]]
 '''
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        subset = []
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            
+            subset.append(nums[i])
+            dfs(i+1)
+            
+            subset.pop()
+            dfs(i+1)
+            
+        dfs(0)
+        return res
