a6e9b127239a7c7207afdb63ba84be82ec189041
shawngonsalves <gonsalveshawn@gmail.com>
2021-06-30 18:28:49 -0700

    Medium_CourseSchedule

--- CourseSchedule.py
+++ CourseSchedule.py
@@ -21,3 +21,32 @@ Explanation: There are a total of 2 courses to take.
 To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
 '''
 
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = {i:[] for i in range(numCourses)}
+        
+        visit = set()
+        
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+            
+        def dfs(crs):
+            if crs in visit:
+                return False
+            if preMap[crs] == []:
+                return True
+            visit.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visit.remove(crs)
+            preMap[crs] = []
+            return True
+            
+        
+        
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True
