217b593963dec0b47a3396d3be05309d434c7560
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-01 23:09:04 -0700

    Easy_VerifyAlienDict

--- VerifyingAlienDictionary.py
+++ VerifyingAlienDictionary.py
@@ -20,3 +20,20 @@ Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
 Output: false
 Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
 '''
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        hashMap = {c:i for i,c in enumerate(order)}
+        
+        for i in range(len(words)-1):
+            w1,w2 = words[i], words[i+1]
+            
+            for j in range(len(w1)):
+                if j ==len(w2):
+                    return False
+                if w1[j] != w2[j]:
+                    if hashMap[w2[j]]< hashMap[w1[j]]:
+                        return False
+                    break
+        return True
+                    
