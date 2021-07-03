a839c6aab49cea70e60361b2cb7ddb0ca0419b6c
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-02 18:37:44 -0700

    Medium_PathSum2

--- PathSum2.py
+++ PathSum2.py
@@ -21,6 +21,29 @@ Example 3:
 Input: root = [1,2], targetSum = 0
 Output: []
 '''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def helper(self, root, sum_tot, lst, result):
+        if root.left is None and root.right is None:
+            if root.val == sum_tot:
+                result += [lst + [root.val]]
+        if root.left:
+                self.helper(root.left, sum_tot - root.val, lst+[root.val], result)
+        if root.right:
+                self.helper(root.right, sum_tot - root.val, lst+[root.val], result)
+                
+        return result   
+        
+        
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        if root is None:
+            return []
+        return self.helper(root, targetSum, [], [])
 
 
 
