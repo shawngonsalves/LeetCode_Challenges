5714ecc89ca9117165b41aa6998f538916fda229
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-02 18:27:53 -0700

    Medium_PathSum2

---
+++ PathSum2.py
@@ -0,0 +1,24 @@
+'''
+Given the root of a binary tree and an integer targetSum, 
+return all root-to-leaf paths where each path's sum equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: []
+'''
+
