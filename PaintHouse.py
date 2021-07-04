d21354c3122bd092f187e62ce341cc523f74eefd
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-03 19:45:17 -0700

    Medium_PaintHouse

--- PaintHouse.py
+++ PaintHouse.py
@@ -22,3 +22,16 @@ Example 2:
 Input: costs = [[7,6,2]]
 Output: 2
 '''
+
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        dp = [0,0,0]
+        
+        for i in range(len(costs)):
+            dp0 = costs[i][0] + min(dp[1], dp[2])
+            dp1 = costs[i][1] + min(dp[0], dp[2])
+            dp2 = costs[i][2] + min(dp[0], dp[1])
+            
+            dp = [dp0, dp1, dp2]
+            
+        return min(dp)
