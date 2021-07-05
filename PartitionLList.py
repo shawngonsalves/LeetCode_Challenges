a2e1a2fc2b0289bf85171fbb8fd2a1d382172de7
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-05 16:23:25 -0700

    Easy_PartitionLL

--- PartitionLList.py
+++ PartitionLList.py
@@ -15,3 +15,24 @@ Example 2:
 Input: head = [2,1], x = 2
 Output: [1,2]
 '''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        lnode, rnode = ListNode(), ListNode()
+        ltail, rtail = lnode, rnode
+        while head:
+            if head.val < x:
+                ltail.next = head
+                ltail = ltail.next
+            else:
+                rtail.next = head
+                rtail = rtail.next
+            head = head.next
+            
+        ltail.next = rnode.next
+        rtail.next = None
+        return lnode.next
