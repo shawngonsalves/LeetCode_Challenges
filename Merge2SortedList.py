36265e665413b46b4a5b53e8d3b109ed64fa971b
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-06 21:32:32 -0700

    Easy_Merge2SortedLL

--- Merge2SortedList.py
+++ Merge2SortedList.py
@@ -17,3 +17,11 @@ Example 3:
 Input: l1 = [], l2 = [0]
 Output: [0]
 '''
+
+class Solution:
+    def mergeTwoLists(self, a, b):
+        if a and b:
+            if a.val > b.val:
+                a, b = b, a
+            a.next = self.mergeTwoLists(a.next, b)
+        return a or b
