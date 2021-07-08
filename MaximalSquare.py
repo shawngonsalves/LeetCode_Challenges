5bb25d5ae927a7b87548fa98bea8235fb592d5d1
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-07 22:36:30 -0700

    Medium_MaximalSquare

---
+++ MaximalSquare.py
@@ -0,0 +1,20 @@
+'''
+Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 4
+Example 2:
+
+
+Input: matrix = [["0","1"],["1","0"]]
+Output: 1
+Example 3:
+
+Input: matrix = [["0"]]
+Output: 0
+'''
