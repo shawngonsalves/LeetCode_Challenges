5fc41b9c325525f558219b13b291528cda2e870e
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-07 23:10:37 -0700

    Medium_MaximalSquare

--- MaximalSquare.py
+++ MaximalSquare.py
@@ -18,3 +18,28 @@ Example 3:
 Input: matrix = [["0"]]
 Output: 0
 '''
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+        #DP with cache bottom up
+        cache = {}
+        
+        def helper(r,c):
+            if r >= ROWS or c >= COLS:
+                return 0
+            
+            if (r,c) not in cache: 
+                down = helper(r+1,c)
+                right = helper(r, c+1)
+                diag = helper(r+1,c+1)
+                
+                cache[(r,c)] = 0
+                if matrix[r][c] == "1":
+                    cache[(r,c)] = 1+ min(down, right, diag)
+                    
+            return cache[(r,c)]
+    
+        helper(0,0)
+        return max(cache.values()) ** 2
