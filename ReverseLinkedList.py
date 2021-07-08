ab4d0343ed1fecfb7de5a8071e3ff2c51c20a464
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-07 22:20:30 -0700

    Easy_Reverse_linkeList

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -16,3 +16,21 @@ Example 3:
 Input: head = []
 Output: []
 '''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+        
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            
+        return prev
+
