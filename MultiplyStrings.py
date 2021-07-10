10f775e4d0816877c633b4aa57df54f7cff10b31
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-09 21:36:14 -0700

    Medium_MultiplyStrings

--- MultiplyStrings.py
+++ MultiplyStrings.py
@@ -13,3 +13,30 @@ Example 2:
 Input: num1 = "123", num2 = "456"
 Output: "56088"
 '''
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if "0" in [num1, num2]:
+            return "0"
+        
+        res = [0]* (len(num1)+ len(num2))
+        
+        num1, num2 = num1[::-1], num2[::-1]
+        
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                digit = int(num1[i]) * int(num2[j])
+                res[i+j] += digit
+                res[i+j+1] +=(res[i+j]//10)
+                res[i+j] = res[i+j] % 10
+
+        print(res)
+        res = res[::-1]
+        beg = 0
+        while beg < len(res) and res[beg] == 0:
+            beg+=1
+            
+        res = map(str, res[beg:])
+        return "".join(res)
+            
+                
