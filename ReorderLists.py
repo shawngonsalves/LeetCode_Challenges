1bf4ee49255ff2141733ca02bbdfb59adb4e3e7a
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-10 11:09:15 -0700

    Medium_ReOrderLL

--- ReorderLists.py
+++ ReorderLists.py
@@ -19,3 +19,36 @@ Example 2:
 Input: head = [1,2,3,4,5]
 Output: [1,5,2,4,3]
 '''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        slow = head
+        fast = head.next
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+            
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1,tmp2
+            
+            
+            
+        
