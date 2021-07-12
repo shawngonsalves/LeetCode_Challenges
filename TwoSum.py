bf83e879306e2fe2e04917f20a7979e78ea84ef0
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-12 00:34:32 -0700

    Easy_TwoSum

--- TwoSum.py
+++ TwoSum.py
@@ -6,3 +6,16 @@ class Solution:
                 if nums[i]+nums[j] == target:
 
                     return([i, j])
+            
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        d = {}
+        
+        for index,value in enumerate(nums):
+            if(target-value in d):
+                return(d[target-value],index)
+            else:
+                d[value] = index
+'''
