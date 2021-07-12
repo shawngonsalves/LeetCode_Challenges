aded4cc293e0cfffd94e22d498ff9b8f6fee2c33
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-12 10:07:39 -0700

    Medium_RotateArray

--- RotateTheArray.py
+++ RotateTheArray.py
@@ -18,3 +18,32 @@ Explanation:
 rotate 1 steps to the right: [99,-1,-100,3]
 rotate 2 steps to the right: [3,99,-1,-100]
 '''
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        k = k % len(nums)
+        
+        def helper(l,r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
+                r-=1
+        l = 0
+        r = len(nums)-1
+        print(nums)
+
+        helper(l,r)
+        print(nums)
+        l = 0
+        r = k-1
+        helper(l,r)
+        print(nums)
+        l = k
+        r = len(nums)-1
+        helper(l,r)
+
+        
