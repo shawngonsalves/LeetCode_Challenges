a191fffffda9ed346a33ee3140b9fbd3b6bb5f33
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-13 09:56:56 -0700

    Medium_RemoveNthNodeFromLL

--- RemoveNthNodefromLL.py
+++ RemoveNthNodefromLL.py
@@ -15,3 +15,27 @@ Example 3:
 Input: head = [1,2], n = 1
 Output: [1]
 '''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0, head)
+        l = dummy
+        r = head
+        
+        while r and n>0:
+            r = r.next
+            n-=1
+            
+        while r:
+            l = l.next
+            r = r.next
+            
+        l.next = l.next.next
+        
+        return dummy.next
+            
