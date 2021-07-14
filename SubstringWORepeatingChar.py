4d66d5137a4cc0ff57335cc689032c04031bc2b0
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-14 12:44:04 -0700

    Medium_LongestSubWORepeatingChar

--- SubstringWORepeatingChar.py
+++ SubstringWORepeatingChar.py
@@ -21,6 +21,23 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
 
 Example 4:
 Input: s = ""
-Output: 0
+Output: 0 Medium_LongestSubWORepeatingChar
 '''
-
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = 0
+        r = 1
+        charSet = set()
+        res = 0
+        
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l+=1
+            charSet.add(s[r])
+            res = max(res, r-l +1)
+            
+        return res
+            
+                
+        
