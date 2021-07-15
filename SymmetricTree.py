cde701c5322566a60020ddd47d93bc1d41ea7336
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-14 18:37:05 -0700

    Easy_SymmetricTree

--- SymmetricTree.py
+++ SymmetricTree.py
@@ -11,3 +11,21 @@ Example 2:
 Input: root = [1,2,2,null,3,null,3]
 Output: false
 '''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root):
+        if not root:
+            return True
+        return self.dfs(root.left, root.right)
+    
+    def dfs(self, l, r):
+        if l and r:
+            return l.val == r.val and self.dfs(l.left, r.right) and self.dfs(l.right, r.left)
+        return l == r
+        
