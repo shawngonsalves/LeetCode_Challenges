fc3fdadf833082d082f01b316386026b5eb5818f
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-15 08:26:30 -0700

    Medium_CombinationSumIV

--- CombinationSumIV.py
+++ CombinationSumIV.py
@@ -25,3 +25,15 @@ Example 2:
 Input: nums = [9], target = 3
 Output: 0
 '''
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = {0:1}
+        
+        for total in range(1, target +1):
+            dp[total] = 0
+            for n in nums:
+                dp[total] += dp.get(total-n,0)
+                
+        return dp[target]
+                
