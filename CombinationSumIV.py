579e3c78d4d23fac7f3e0f0e34fbbd3ecc78a58b
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-15 08:26:01 -0700

    CombinationSumIV

---
+++ CombinationSumIV.py
@@ -0,0 +1,27 @@
+'''
+Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+The answer is guaranteed to fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3], target = 4
+Output: 7
+Explanation:
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+Note that different sequences are counted as different combinations.
+
+Example 2:
+
+Input: nums = [9], target = 3
+Output: 0
+'''
