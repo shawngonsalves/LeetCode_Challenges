9577f04c1ebe97f98f0a113e02b2c4219ecb1379
shawngonsalves <gonsalveshawn@gmail.com>
2021-07-18 14:42:45 -0700

    Medium_InsertInterval

--- InsertIntervals.py
+++ InsertIntervals.py
@@ -27,3 +27,21 @@ Example 5:
 Input: intervals = [[1,5]], newInterval = [2,7]
 Output: [[1,7]]
 '''
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        
+        for i in range(len(intervals)):
+            
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res+ intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+                
+            else:
+                newInterval = [min(newInterval[0],intervals[i][0]), max(newInterval[1],intervals[i][1])]
+                
+        res.append(newInterval)
+        return res
