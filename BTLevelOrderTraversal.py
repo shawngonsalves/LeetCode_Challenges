e80738cf361c0f7a22e9efcce42949ba4b5cf19f
Shawn Gonsalves <55061053+shawngonsalves@users.noreply.github.com>
2023-03-18 01:29:03 -0700

    Medium_BinaryTreeLevelOrderTraversal
    Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
    
    
    Example 1:
    
    Input: root = [3,9,20,null,null,15,7]
    Output: [[3],[9,20],[15,7]]
    
    Example 2:
    Input: root = [1]
    Output: [[1]]

---
+++ BTLevelOrderTraversal.py
@@ -0,0 +1,18 @@
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        q = deque()
+        q.append(root)
+
+        while q:
+            level = []
+            ql = len(q)
+            for i in range(ql):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
