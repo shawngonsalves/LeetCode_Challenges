97e0f3b5d38091f1831f0d5c9785e77a79e3cfcd
Shawn Gonsalves <55061053+shawngonsalves@users.noreply.github.com>
2023-03-19 23:28:17 -0700

    Medium_Permutations
    Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
    
    
    Example 1:
    Input: nums = [1,2,3]
    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
    
    Example 2:
    Input: nums = [0,1]
    Output: [[0,1],[1,0]]
    
    Example 3:
    Input: nums = [1]
    Output: [[1]]

---
+++ Permutations.py
@@ -0,0 +1,15 @@
+        res = []
+
+        # base case
+        if len(nums) == 1:
+            return [nums[:]]  # nums[:] is a deep copy
+
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for perm in perms:
+                perm.append(n)
+            res.extend(perms)
+            nums.append(n)
+        return res
