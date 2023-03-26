b7f9f860797d705413779fde10054ef0f01c2fdf
Shawn Gonsalves <55061053+shawngonsalves@users.noreply.github.com>
2023-03-26 15:30:49 -0700

    Medium_WallsAndGates_Premium
    Description
    You are given a m x n 2D grid initialized with these three possible values.
    
    -1 - A wall or an obstacle.
    0 - A gate.
    INF - Infinity means an empty room. We use the value 2^31 - 1 = 2147483647 to represent INF as you may assume that the distance to a gate is less than 2147483647.
    Fill each empty room with the distance to its nearest gate. If it is impossible to reach a Gate, that room should remain filled with INF
    
    
    
    Example1
    
    Input:
    [[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
    Output:
    [[3,-1,0,1],[2,2,1,-1],[1,-1,2,-1],[0,-1,3,4]]
    
    Explanation:
    the 2D grid is:
    INF  -1  0  INF
    INF INF INF  -1
    INF  -1 INF  -1
      0  -1 INF INF
    the answer is:
      3  -1   0   1
      2   2   1  -1
      1  -1   2  -1
      0  -1   3   4
    
    
    
    Example2
    
    Input:
    [[0,-1],[2147483647,2147483647]]
    Output:
    [[0,-1],[1,2]]

--- WallsAndGates.py
+++ WallsAndGates.py
@@ -1,64 +1,33 @@
-'''
-You are given an m x n grid rooms initialized with these three possible values.
-
--1 A wall or an obstacle.
-0 A gate.
-INF Infinity means an empty room. We use the value 231 - 1 = 2147483647 to represent INF as you may assume that the distance to a gate is less than 2147483647.
-Fill each empty room with the distance to its nearest gate. If it is impossible to reach a gate, it should be filled with INF.
-
- 
-
-Example 1:
-
-
-Input: rooms = [[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
-Output: [[3,-1,0,1],[2,2,1,-1],[1,-1,2,-1],[0,-1,3,4]]
-Example 2:
-
-Input: rooms = [[-1]]
-Output: [[-1]]
-Example 3:
-
-Input: rooms = [[2147483647]]
-Output: [[2147483647]]
-'''
-
 class Solution:
-    def wallsAndGates(self, rooms: List[List[int]]) -> None:
-        """
-        Do not return anything, modify rooms in-place instead.
-        """
+    """
+    @param rooms: m x n 2D grid
+    @return: nothing
+    """
+    def walls_and_gates(self, rooms: List[List[int]]):
+        # write your code here
         ROWS, COLS = len(rooms), len(rooms[0])
+        q = collections.deque()
         visit = set()
-        q = deque()
-        
-        def addRoom(r,c):
-            if (r<0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or rooms[r][c] == -1):
+
+        def addRoom(r, c):
+            if (r < 0 or c < 0 or r == ROWS or c < 0 or c== COLS or (r,c) in visit or rooms[r][c] == -1):
                 return
             visit.add((r,c))
             q.append([r,c])
-            
-        
-        
+
+
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
                     q.append([r,c])
                     visit.add((r,c))
-        
         dist = 0
         while q:
             for i in range(len(q)):
                 r, c = q.popleft()
-                
                 rooms[r][c] = dist
-                addRoom(r+1, c)
-                addRoom(r-1, c)
-                addRoom(r, c+1)
-                addRoom(r, c-1)
-            dist +=1
-            
-'''
-Runtime: 280 ms, faster than 64.99% of Python3 online submissions for Walls and Gates.
-Memory Usage: 20.8 MB, less than 23.09% of Python3 online submissions for Walls and Gates.
-'''
+                addRoom(r+1,c)
+                addRoom(r-1,c)
+                addRoom(r,c+1)
+                addRoom(r,c-1)
+            dist += 1
