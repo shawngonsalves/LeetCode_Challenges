e7495507e3c99587512f8afbe35d2b80656dd1b7
Shawn Gonsalves <55061053+shawngonsalves@users.noreply.github.com>
2023-04-02 23:27:00 -0700

    Medium_InterleavingString-Memoization
    Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
    
    An interleaving of two strings s and t is a configuration where s and t are divided into n and m 
    substrings
     respectively, such that:
    
    s = s1 + s2 + ... + sn
    t = t1 + t2 + ... + tm
    |n - m| <= 1
    The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
    Note: a + b is the concatenation of strings a and b.
    
     
    
    Example 1:
    
    Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
    Output: true
    Explanation: One way to obtain s3 is:
    Split s1 into s1 = "aa" + "bc" + "c", and s2 into s2 = "dbbc" + "a".
    Interleaving the two splits, we get "aa" + "dbbc" + "bc" + "a" + "c" = "aadbbcbcac".
    Since s3 can be obtained by interleaving s1 and s2, we return true.
    
    
    Example 2:
    
    Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
    Output: false
    Explanation: Notice how it is impossible to interleave s2 with any other string to obtain s3.
    
    
    Example 3:
    
    Input: s1 = "", s2 = "", s3 = ""
    Output: true

---
+++ InterleavingString.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        dp = {}
+
+        def dfs(i, j):
+            if (i, j) in dp:
+                return dp[(i, j)]
+            if i == len(s1) and j == len(s2):
+                return True
+            if i < len(s1) and s1[i] == s3[i+j] and dfs(i+1, j):
+                return True
+            if j < len(s2) and s2[j] == s3[i+j] and dfs(i, j+1):
+                return True
+            dp[(i, j)] = False
+            return False
+        return dfs(0, 0)
