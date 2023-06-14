1c81bced76cc6ba3ef2ad6d5e11acef3b29f0c9e
Shawn Gonsalves <55061053+shawngonsalves@users.noreply.github.com>
2023-06-14 04:40:21 +0000

    Medium_1376

--- RotateTheArray.py
+++ RotateTheArray.py
@@ -29,7 +29,7 @@ class Solution:
         
         def helper(l,r):
             while l < r:
-                nums[l], nums[r] = nums[r], nums[l]
+                nums[l], nums[r] = nums[r],nums[l]
                 l+=1
                 r-=1
         l = 0
