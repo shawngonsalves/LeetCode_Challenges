853c05d49389f77f938c7dafd1c5825b9725852c
Shawn Gonsalves <55061053+shawngonsalves@users.noreply.github.com>
2023-07-15 05:57:31 +0000

    UpdatedString solution in python

--- ReverseString.py
+++ ReverseString.py
@@ -1,5 +1,36 @@
+# Write a function that reverses a string. The input string is given as an array of characters s.
+
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+# Example 1:
+
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+
+# Example 2:
+
+# Input: s = ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 105
+# s[i] is a printable ascii character.
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s.reverse()
+        # s.reverse()
 
-        
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1        
+
+# Runtime 227 ms Beats 35.85%
+# Memory 20.7 MB Beats 56.59 
