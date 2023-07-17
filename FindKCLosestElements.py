904771afaa5bcb402acb7a0fb98c5d8a1c4f83f4
Shawn Gonsalves <55061053+shawngonsalves@users.noreply.github.com>
2023-07-17 07:31:02 +0000

    FindKClosestElements

---
+++ FindKCLosestElements.py
@@ -0,0 +1,34 @@
+# Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+# An integer a is closer to x than an integer b if:
+
+# |a - x| < |b - x|, or
+# |a - x| == |b - x| and a < b
+ 
+
+# Example 1:
+
+# Input: arr = [1,2,3,4,5], k = 4, x = 3
+# Output: [1,2,3,4]
+# Example 2:
+
+# Input: arr = [1,2,3,4,5], k = 4, x = -1
+# Output: [1,2,3,4]
+
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left = 0
+        right = len(arr) - 1
+        
+        while right - left >= k:
+            if abs(x - arr[left]) > abs(x - arr[right]):
+                left += 1
+            else:
+                right -= 1
+        
+        return (arr[left:right+1])
+
+
+# Runtime 295 ms Beats 70.75%
+# Memory 17.8 MB Beats 82.4%
