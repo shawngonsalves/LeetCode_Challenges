2c6f922d1c35bbe4a874144f3e625cb1015e00f6
Shawn Gonsalves <55061053+shawngonsalves@users.noreply.github.com>
2023-07-20 04:10:43 +0000

    Medium Remove K Adjacent Duplicates

---
+++ RemoveKAdjacentDups.py
@@ -0,0 +1,54 @@
+# You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
+
+# We repeatedly make k duplicate removals on s until we no longer can.
+
+# Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+ 
+
+# Example 1:
+
+# Input: s = "abcd", k = 2
+# Output: "abcd"
+# Explanation: There's nothing to delete.
+# Example 2:
+
+# Input: s = "deeedbbcccbdaa", k = 3
+# Output: "aa"
+# Explanation: 
+# First delete "eee" and "ccc", get "ddbbbdaa"
+# Then delete "bbb", get "dddaa"
+# Finally delete "ddd", get "aa"
+# Example 3:
+
+# Input: s = "pbbcggttciiippooaais", k = 2
+# Output: "ps"
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 105
+# 2 <= k <= 104
+# s only contains lowercase English letters.
+
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = []
+
+        for c in s:
+            if stack and stack[-1][0] == c:
+                stack[-1][1] += 1
+            else:
+                stack.append([c, 1])
+            if stack[-1][1] == k:
+                stack.pop()
+
+
+        res = ""
+        for c, count in stack:
+            res += c*count
+        return res
+
+
+# Runtime 127 ms Beats 52.87%
+# Memory 21.2 MB Beats 31.60%
