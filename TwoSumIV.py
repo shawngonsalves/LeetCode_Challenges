9325dda9f08511d76c01f0f01d3701f51e91b5a6
shawngonsalves <gonsalveshawn@gmail.com>
2023-11-03 14:31:55 -0400

    Easy_TwoSum-IV

---
+++ TwoSumIV.py
@@ -0,0 +1,51 @@
+'''
+Given the root of a binary search tree and an integer k, return true if there exist two elements in the BST such that their sum is equal to k, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 9
+Output: true
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 28
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-104 <= Node.val <= 104
+root is guaranteed to be a valid binary search tree.
+-105 <= k <= 105
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        l = set()
+
+        def dfs(node):
+            if not node:
+                return False
+            y = k - node.val
+            if y in l:
+                return True
+            else:
+                l.add(node.val)
+            return dfs(node.left) or dfs(node.right)
+        
+        return dfs(root)
+
+'''
+Runtime 59 ms Beats 98.70%
+Memory 21.3 MB Beats 44.52%
+'''
