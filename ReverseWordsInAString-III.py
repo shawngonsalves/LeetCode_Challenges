ff66dc6fc26f363b344cd838dbafdf718c27186c
shawngonsalves <gonsalveshawn@gmail.com>
2023-11-13 10:57:05 -0500

    Easy_ReverseWords-in-StringIII

---
+++ ReverseWordsInAString-III.py
@@ -0,0 +1,40 @@
+'''
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+ 
+
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+Example 2:
+
+Input: s = "God Ding"
+Output: "doG gniD"
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+s contains printable ASCII characters.
+s does not contain any leading or trailing spaces.
+There is at least one word in s.
+All the words in s are separated by a single space.
+'''
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        split = s.split(" ")
+        output = ''
+        for word in split:
+            for letter in range(len(word)-1, -1, -1):
+                output += word[letter]
+            
+            output += " "
+        
+        return str(output[:-1])
+
+'''
+Runtime 59 ms Beats 21.83%
+Memory 16.9 MB Beats 79.72%
+'''
