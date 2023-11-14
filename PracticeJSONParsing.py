a7ef3e266e1ac116c479cd2e3056cb8b066f1a87
shawngonsalves <gonsalveshawn@gmail.com>
2023-11-14 10:10:31 -0500

    Practice JSON

--- PracticeJSONParsing.py
+++ PracticeJSONParsing.py
@@ -1,9 +1,39 @@
 import json
 
 from urllib.request import urlopen
-json.dumps(tempfile, f)
+# json.dumps(tempfile, f)
 
-with open('file.py', 'w') as f:
+# with open('file.py', 'w') as f:
 
-with urlopen("url") as response:
-    source = response.read()
+# with urlopen("url") as response:
+#     source = response.read()
+
+
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+
+
+output = json.dumps(x)
+
+new_op = json.loads(output)
+#print(new_op)
+
+for k, v in new_op['cars']:
+    for c in k:
+        print(c)
+
+
+#print(output)
