e4319d3fae54ebaea95b3e8cb86e0c306dbe4518
shawngonsalves <gonsalveshawn@gmail.com>
2023-11-20 14:37:46 -0500

    JSON_parsing

--- PracticeJSONParsing.py
+++ PracticeJSONParsing.py
@@ -1,6 +1,18 @@
 import json
 
 from urllib.request import urlopen
+import openurl
+
+with openurl("http://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json") as response:
+    source = response.read()
+
+# with open('file.py', 'w') as f:
+
+data = json.loads(source)
+print(data)
+
+for item in data['']['']:
+    name = item['']['']['name']
 # json.dumps(tempfile, f)
 
 # with open('file.py', 'w') as f:
@@ -9,31 +21,33 @@ from urllib.request import urlopen
 #     source = response.read()
 
 
-import json
+# import json
 
-x = {
-  "name": "John",
-  "age": 30,
-  "married": True,
-  "divorced": False,
-  "children": ("Ann","Billy"),
-  "pets": None,
-  "cars": [
-    {"model": "BMW 230", "mpg": 27.5},
-    {"model": "Ford Edge", "mpg": 24.1}
-  ]
-}
+# x = {
+#   "name": "John",
+#   "age": 30,
+#   "married": True,
+#   "divorced": False,
+#   "children": ("Ann","Billy"),
+#   "pets": None,
+#   "cars": [
+#     {"model": "BMW 230", "mpg": 27.5},
+#     {"model": "Ford Edge", "mpg": 24.1}
+#   ]
+# }
 
 
 
-output = json.dumps(x)
+# output = json.dumps(x)
 
-new_op = json.loads(output)
-#print(new_op)
+# new_op = json.loads(output)
+# #print(new_op)
 
-for k, v in new_op['cars']:
-    for c in k:
-        print(c)
+# for k, v in new_op['cars']:
+#     for c in k:
+#         print(c)
 
 
 #print(output)
+
+
