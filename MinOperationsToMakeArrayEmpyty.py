3ee176d519f5873787c2501e902532d11fec2ff9
shawngonsalves <gonsalveshawn@gmail.com>
2024-01-04 16:51:51 -0500

    Medium_MinOperationsToMakeArrEmpty

---
+++ MinOperationsToMakeArrayEmpyty.py
@@ -0,0 +1,60 @@
+'''
+You are given a 0-indexed array nums consisting of positive integers.
+
+There are two types of operations that you can apply on the array any number of times:
+
+Choose two elements with equal values and delete them from the array.
+Choose three elements with equal values and delete them from the array.
+Return the minimum number of operations required to make the array empty, or -1 if it is not possible.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,3,2,2,4,2,3,4]
+Output: 4
+Explanation: We can apply the following operations to make the array empty:
+- Apply the first operation on the elements at indices 0 and 3. The resulting array is nums = [3,3,2,4,2,3,4].
+- Apply the first operation on the elements at indices 2 and 4. The resulting array is nums = [3,3,4,3,4].
+- Apply the second operation on the elements at indices 0, 1, and 3. The resulting array is nums = [4,4].
+- Apply the first operation on the elements at indices 0 and 1. The resulting array is nums = [].
+It can be shown that we cannot make the array empty in less than 4 operations.
+Example 2:
+
+Input: nums = [2,1,2,2,3,3]
+Output: -1
+Explanation: It is impossible to empty the array.
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+1 <= nums[i] <= 106
+'''
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        """
+        1 -> -1
+        2 -> 1
+        3-> 1
+        4-> 2
+        5-> 2 +3
+        6 -> 3+3
+        7 -> 2+2+3
+        8 -> 2+3+3
+        9 -> 3+3+3...
+
+        """
+        count = Counter(nums)
+        res = 0
+        for n, c in count.items():
+            if c == 1:
+                return -1
+            res += math.ceil(c / 3)
+        return res
+    
+'''
+Runtime 530 ms Beats 98.77%
+Memory 32.3 MB Beats 7.10%
+'''
