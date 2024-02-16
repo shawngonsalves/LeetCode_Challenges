282463133a577b9445d4db33c90ea21efe2df569
shawngonsalves <gonsalveshawn@gmail.com>
2024-02-16 01:42:09 -0500

    Medium_LeastNoOfUniqueIntegersAdterKRemovals- Sol 2

--- LeastNoOfUniqueIntegegersAfterKRemovals.py
+++ LeastNoOfUniqueIntegegersAfterKRemovals.py
@@ -41,4 +41,30 @@ Runtime 359 ms Beats 55.66% of users with Python3
 Memory 33.39 MB Beats 84.81% of users with Python3
 '''
 
-            
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        count = Counter(arr)     
+
+        freq_list = [0] * (len(arr)+ 1) # freq -:> #of elements w that freq
+
+        for f in count.values():
+            freq_list[f] += 1
+        res = len(count)
+
+        for f in range(1, len(freq_list)):
+            remove = freq_list[f]
+
+            if k >= f * remove:
+                k -= f * remove
+                res -= remove
+            else:
+                remove = k // f
+                res -= remove
+                break
+        return res
+
+'''
+Runtime 316 ms Beats 92.47% of users with Python3
+Memory 33.31 MB Beats 84.81% of users with Python3
+
+'''
