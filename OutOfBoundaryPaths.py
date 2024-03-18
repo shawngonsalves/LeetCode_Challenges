3e11b0b90307e27f79458c1ea2dcd94f204675fd
shawngonsalves <gonsalveshawn@gmail.com>
2024-03-18 11:58:53 -0400

    Medium_OutOfBoundaryPaths - Bottom-up DP

--- OutOfBoundaryPaths.py
+++ OutOfBoundaryPaths.py
@@ -48,3 +48,38 @@ class Solution:
 Runtime 81 ms Beats 71.37% of users with Python3
 Memory 21.96 MB Beats 45.38% of users with Python3
 '''
+
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        ROWS, COLS = m, n
+        MOD = 10**9 + 7
+        grid = [[0]*COLS for r in range(ROWS)]
+
+        for m in range(1, maxMove + 1):
+            tmp = [[0]*COLS for r in range(ROWS)]
+            for r in range(ROWS):
+                for c in range(COLS):
+                    if r-1 < 0:
+                        tmp[r][c] = (tmp[r][c] + 1) % MOD
+                    else:
+                        tmp[r][c] = (tmp[r][c] + grid[r-1][c]) % MOD
+                    if r+1 == ROWS:
+                        tmp[r][c] = (tmp[r][c] + 1) % MOD
+                    else:
+                         tmp[r][c] = (tmp[r][c] + grid[r+1][c]) % MOD
+                    if c-1 < 0:
+                        tmp[r][c] = (tmp[r][c] + 1) % MOD
+                    else:
+                        tmp[r][c] = (tmp[r][c] + grid[r][c-1]) % MOD
+                    if c+1 == COLS:
+                        tmp[r][c] = (tmp[r][c] + 1) % MOD
+                    else:
+                        tmp[r][c] = (tmp[r][c] + grid[r][c+1]) % MOD
+            grid = tmp    
+        return grid[startRow][startColumn]       
+
+
+'''
+Runtime 139 ms Beats 13.50% of users with Python3
+Memory 16.86 MB Beats 79.89% of users with Python3
+'''
